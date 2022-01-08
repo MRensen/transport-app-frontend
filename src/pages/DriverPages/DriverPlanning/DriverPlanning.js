957dab0d2295cad78a037149190f5796c7beee64
MRensen <markrensen@hotmail.com>
2022-01-08 21:18:42 +0100

    finished planning page, updated backend (Pallets and Orders)

--- src/pages/DriverPages/DriverPlanning/DriverPlanning.js
+++ src/pages/DriverPages/DriverPlanning/DriverPlanning.js
@@ -3,30 +3,43 @@ import {HeaderHomeSave} from "../../../components/Header/Header";
 import {useContext, useEffect, useState} from "react";
 import axios from "axios";
 import {RouteContext} from "../../../components/Context/RouteContextProvider";
+import OrderContainer from "../../../components/OrderContainer/OrderContainer";
+import {useHistory} from "react-router-dom";
 
 export default function DriverPlanning(){
-    const[routeData, setRouteData] = useState({});
-    const {data} = useContext(RouteContext);
-    useEffect(()=>{
-        async function getRouteData(){
-            try {
-                const result = await axios.get("http://localhost:8080/")
-            } catch (e){
+    const[orderData, setOrderData] = useState({})
+    const[endpoint, setEndpoint] = useState("");
+    const {routeData} = useContext(RouteContext);
+    const history = useHistory();
 
-            }
-        }
-    },[])
+    // useEffect(()=>{
+    //     async function getRouteData(){
+    //         try {
+    //             const result = await axios.get(`http://localhost:8080/orders/${endpoint}`)
+    //             setOrderData(result);
+    //         } catch (e){
+    //             console.log("error fetching data: " + e.error);
+    //         }
+    //     }
+    //     getRouteData();
+    // },[endpoint])
+    function leftFunction(){
+        console.log("home")
+        history.push("/driver/home")
+    };
     return(
+
         <>
             <HeaderHomeSave
                 titleName="planning"
-                leftFunction={console.log("home")}
+                leftFunction={leftFunction}
                 disableSave = {true}
                 />
             <main className={styles.name}>
-                <article className={styles.article}>
+                    {routeData && routeData.orders.map((order)=>{
+                        return <OrderContainer key={order.id} endpoint={order.id} />
 
-                </article>
+                    })}
             </main>
         </>
     )
