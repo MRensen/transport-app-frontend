14ec03bd3ebcc7188b765bf63fb6c1610bbfe2a8
MRensen <markrensen@hotmail.com>
2022-01-11 10:55:22 +0100

    added customeHomeButton to customerHome

---
+++ src/components/CustomerHomeButton/CustomerHomeButton.js
@@ -0,0 +1,14 @@
+import styles from "./CustomerHomeButton.module.css";
+import {useHistory} from "react-router-dom";
+export default function CustomerHomeButton({path, name, logo}){
+    const history = useHistory();
+    return(
+    <div className={styles.button}>
+        <input type="image" src={logo} id={name} onClick={() => {
+            console.log(name);
+            history.push(path);
+        }}/>
+        <label htmlFor={name} className={styles.label}> {name} </label>
+    </div>
+    )
+}
--- src/pages/CustomerPages/CustomerHome/CustomerHome.js
+++ src/pages/CustomerPages/CustomerHome/CustomerHome.js
@@ -6,6 +6,7 @@ import ordersLogo from "../../../resources/mijnOrdersLogo.png";
 import nieuwOrderLogo from "../../../resources/nieuweOrderLogo.png";
 import accountLogo from "../../../resources/mijnAccountLogo.png";
 import {useHistory} from "react-router-dom";
+import CustomerHomeButton from "../../../components/CustomerHomeButton/CustomerHomeButton";
 
 export default function CustomerHome() {
     const {data} = useContext(AuthContext);
@@ -17,27 +18,30 @@ export default function CustomerHome() {
             <main className={styles.main}>
                 <h1 className={styles.title}>{data.name}</h1>
                 <div className={styles['button-container']}>
-                    <div className={styles.button}>
-                        <input type="image" src={nieuwOrderLogo} id="nieuwe order" onClick={() => {
-                            console.log("nieuwe order");
-                            history.push("/customer/new");
-                        }}/>
-                        <label htmlFor="nieuwe order" className={styles.label}> nieuwe order </label>
-                    </div>
-                    <div className={styles.button}>
-                        <input type="image" src={ordersLogo} id="mijn orders" onClick={() => {
-                            console.log("mijn orders");
-                            history.push("/customer/orders");
-                        }}/>
-                        <label htmlFor="mijn orders" className={styles.label}> mijn orders </label>
-                    </div>
-                    <div className={styles.button}>
-                        <input type="image" src={accountLogo} id="nieuwe account" onClick={() => {
-                            console.log("mijn account");
-                            history.push("/customer/account");
-                        }}/>
-                        <label htmlFor="mijn account" className={styles.label}> mijn account</label>
-                    </div>
+                    <CustomerHomeButton path="/customer/account" name="mijn account" logo={accountLogo}/>
+                    <CustomerHomeButton path="/customer/new" name="nieuwe order" logo={nieuwOrderLogo}/>
+                    <CustomerHomeButton path="customer/orders" name="mijn orders" logo={ordersLogo}/>
+                    {/*<div className={styles.button}>*/}
+                    {/*    <input type="image" src={nieuwOrderLogo} id="nieuwe order" onClick={() => {*/}
+                    {/*        console.log("nieuwe order");*/}
+                    {/*        history.push("/customer/new");*/}
+                    {/*    }}/>*/}
+                    {/*    <label htmlFor="nieuwe order" className={styles.label}> nieuwe order </label>*/}
+                    {/*</div>*/}
+                    {/*<div className={styles.button}>*/}
+                    {/*    <input type="image" src={ordersLogo} id="mijn orders" onClick={() => {*/}
+                    {/*        console.log("mijn orders");*/}
+                    {/*        history.push("/customer/orders");*/}
+                    {/*    }}/>*/}
+                    {/*    <label htmlFor="mijn orders" className={styles.label}> mijn orders </label>*/}
+                    {/*</div>*/}
+                    {/*<div className={styles.button}>*/}
+                    {/*    <input type="image" src={accountLogo} id="mijn account" onClick={() => {*/}
+                    {/*        console.log("mijn account");*/}
+                    {/*        history.push("/customer/account");*/}
+                    {/*    }}/>*/}
+                    {/*    <label htmlFor="mijn account" className={styles.label}> mijn account</label>*/}
+                    {/*</div>*/}
 
                 </div>
             </main>
