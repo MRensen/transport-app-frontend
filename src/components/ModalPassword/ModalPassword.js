7db25b8aa53bf62166f6a6e594a07f92b7c3c771
MRensen <markrensen@hotmail.com>
2022-01-22 22:24:34 +0100

    disabled password change (it changes in db, but user cannot log in)

--- src/components/ModalPassword/ModalPassword.js
+++ src/components/ModalPassword/ModalPassword.js
@@ -7,11 +7,8 @@ import axios from "axios";
 export default function ModalPassword({ show, onClose, id: username}) {
     const { loggedIn} = useContext(AuthContext);
     const {register, handleSubmit} = useForm()
-    const [password, setPassword] = useState("")
 
-
-     useEffect(()=>{
-        async function changePassword(){
+        async function changePassword(password){
             console.log(username)
             try {
                 await axios({
@@ -20,18 +17,16 @@ export default function ModalPassword({ show, onClose, id: username}) {
                     data: {password: password}
                     //TODO axios headers
                 })
+                console.log("password changed")
             }catch (e){
                 console.log(e.message)}
         }
-        changePassword();
-        console.log("password changed")
-    },[password])
 
 
     async function handlePassword(form){
         if(loggedIn){
             if(form.new === form.again){
-                setPassword(form.new);
+                await changePassword(form.new);
             } else {window.alert("new passwords do not match")}
         } else {window.alert("current password is not correct")}
         onClose();
@@ -41,39 +36,49 @@ export default function ModalPassword({ show, onClose, id: username}) {
     if(!show){
         return null;
     }
+    if(true){
+        return(
+            <div className={styles.modal} onClick={onClose}>
+                <main className={styles.main} onClick={e => e.stopPropagation()}>
+                    <p>Soon to be implemented</p>
+                </main>
+            </div>
+        )
+    } else {
+        return (
+            <div className={styles.modal} onClick={onClose}>
+                <main className={styles.main} onClick={e => e.stopPropagation()}>
+                    <header>
+                        <h3 className={styles.h3}> change password</h3>
+                    </header>
+                    <article className={styles.article}>
+                        <form onSubmit={handleSubmit(handlePassword)}>
+                            <label htmlFor="new" className={styles.label}> enter new password:
+                                <input
+                                    type="password"
+                                    id="new"
+                                    {...register("new")}
+                                />
+                            </label>
+                            <label htmlFor="again" className={styles.label}> repeat new password:
+                                <input
+                                    type="password"
+                                    id="again"
+                                    {...register("again")}
+                                />
+                            </label>
 
-    return (
-        <div className={styles.modal} onClick={onClose}>
-            <main className={styles.main} onClick={e => e.stopPropagation()}>
-                <header>
-                    <h3 className={styles.h3}> change password</h3>
-                </header>
-                <article className={styles.article}>
-                    <form onSubmit={handleSubmit(handlePassword)}>
-                        <label htmlFor="new" className={styles.label}> enter new password:
-                            <input
-                                type="password"
-                                id="new"
-                                {...register("new")}
-                            />
-                        </label>
-                        <label htmlFor="again" className={styles.label}> repeat new password:
-                            <input
-                                type="password"
-                                id="again"
-                                {...register("again")}
-                            />
-                        </label>
+                        </form>
 
-                    </form>
-
-                </article>
-                <footer className={styles.footer}>
-                    <button className={styles.button}
-                            onClick={handleSubmit(handlePassword)}
-                    >Save</button>
-                </footer>
-            </main>
-        </div>
-    )
+                    </article>
+                    <footer className={styles.footer}>
+                        <button className={styles.button}
+                                onClick={handleSubmit(handlePassword)}
+                        >Save
+                        </button>
+                    </footer>
+                </main>
+            </div>
+        )
+    }
 }
