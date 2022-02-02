f7a1ec5d286d1b5fae034af76f37f908475a0b19
MRensen <markrensen@hotmail.com>
2022-02-02 11:30:12 +0100

    extracted methods

--- src/components/LabeledInput/LabeledInput.js
+++ src/components/LabeledInput/LabeledInput.js
@@ -1,42 +1,56 @@
 import styles from "./LabeledInput.module.css";
 
+function CheckboxLabeledInput(title, value, className, register, children) {
+    return (
+        <label className={styles.label} htmlFor={`${title}: ${value}`}>
+            {title}: <input className={styles[className]} type="checkbox" id={`${title}: ${value}`}
+                            onChange={onchange}
+                            {...register(title)}
+        /> {children}
+        </label>
+    )
+}
+
+function LabeledInputWithErrors(title, value, className, type, register, children, errors) {
+    const options = {required: true};
+    if (title === "personeels nummer") {
+        options.pattern = /^[0-9]*$/
+    }
+    return (
+        <label key={title} className={styles.label} htmlFor={`${title}: ${value}`}>
+            {title}: <input className={styles[className]} type={type} id={`${title}: ${value}`}
+                            key={title}
+                            {...register(title, options)}
+        /> {children}
+            {(Object.keys(errors).length !== 0) &&
+            <p style={{color: '#FF0000'}}>
+                {errors[title]?.type === "required" && "required"} <br/>
+                {errors[title]?.type === "pattern" && "only digits"}
+            </p>
+            }
+        </label>
+    )
+}
+
+function BasicLabeledInput(title, value, className, type, register, children) {
+    return (
+        <label key={title} className={styles.label} htmlFor={`${title}: ${value}`}>
+            {title}: <input className={styles[className]} type={type} id={`${title}: ${value}`}
+                            key={title}
+                            value={value}
+                            {...register(title)}
+        /> {children}
+        </label>
+    )
+}
+
 export default function LabeledInput({children, type = "text", title, value, className, register, errors, checked, onChange }){
     if(checked){
-        return (
-            <label className={styles.label} htmlFor={`${title}: ${value}`}>
-                {title}: <input className={styles[className]} type="checkbox" id={`${title}: ${value}`}
-                                onChange={onchange}
-                                {...register(title)}
-            /> {children}
-            </label>
-        )
+        return CheckboxLabeledInput(title, value, className, register, children);
     }
     if(errors) {
-        const options = {required: true};
-        if(title === "personeels nummer"){options.pattern = /^[0-9]*$/}
-        return (
-            <label key={title} className={styles.label} htmlFor={`${title}: ${value}`}>
-                {title}: <input className={styles[className]} type={type} id={`${title}: ${value}`}
-                                key={title}
-                                {...register(title, options)}
-            /> {children}
-                {(Object.keys(errors).length !== 0) &&
-                <p style={{color: '#FF0000'}}>
-                    {errors[title]?.type === "required" && "required"} <br/>
-                    {errors[title]?.type === "pattern" && "only digits"}
-                </p>
-                }
-            </label>
-        )
+        return LabeledInputWithErrors(title, value, className, type, register, children, errors);
     } else {
-        return (
-            <label key={title} className={styles.label} htmlFor={`${title}: ${value}`}>
-                {title}: <input className={styles[className]} type={type} id={`${title}: ${value}`}
-                                key={title}
-                                value={value}
-                                {...register(title)}
-            /> {children}
-            </label>
-        )
+        return BasicLabeledInput(title, value, className, type, register, children);
     }
 }
